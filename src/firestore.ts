e1ae07f0ec75a26c94e0b00288442f6ae91e20b2
Cam <cameron.soulsby@gmail.com>
2023-04-14 16:19:09 +1200

    Fix bug causing goals to not update

--- src/firestore.ts
+++ src/firestore.ts
@@ -7,7 +7,7 @@ import {
   doc,
   deleteDoc,
   setDoc,
-  addDoc
+  addDoc,
 } from "firebase/firestore";
 
 //create
@@ -19,8 +19,7 @@ export const createUser = async (
   const docRef = doc(db, "users", newUid);
   const data = { email: newEmail, username: newUsername };
   await setDoc(docRef, data);
-
-}
+};
 
 //read
 export const getUsername = async (userId: string) => {
@@ -33,8 +32,7 @@ export const getUsername = async (userId: string) => {
   } else {
     return "null";
   }
-}
-
+};
 
 export const getUsers = async () => {
   const usersCollectionRef = collection(db, "users");
@@ -61,9 +59,14 @@ export const getDisciplines = async (userId: string) => {
     };
   });
   return mappedData;
-}
+};
 
-export const getTotalRepsForPeriod = async (userId: string, disciplineId: string, startDate: Date, endDate: Date) => {
+export const getTotalRepsForPeriod = async (
+  userId: string,
+  disciplineId: string,
+  startDate: Date,
+  endDate: Date
+) => {
   const userDoc = doc(db, "users", userId);
   const disciplinesRef = collection(userDoc, "disciplines");
   const disciplineDoc = doc(disciplinesRef, disciplineId);
@@ -77,33 +80,49 @@ export const getTotalRepsForPeriod = async (userId: string, disciplineId: string
       timeStamp: setData.timeStamp || new Date(),
     };
   });
-  
-  const filteredData = mappedData.filter((set) => set.timeStamp.toDate() >= startDate && set.timeStamp.toDate() <= endDate);
+
+  const filteredData = mappedData.filter(
+    (set) =>
+      set.timeStamp.toDate() >= startDate && set.timeStamp.toDate() <= endDate
+  );
   const totalReps = filteredData.reduce((acc, set) => acc + set.reps, 0);
   return totalReps;
-}
+};
 
 interface DataPoint {
-  timeStamp: Date
+  timeStamp: Date;
 }
 
-export const getMostRecentSetDate = async (userId: string, disciplineId: string) => {
+export const getMostRecentSetDate = async (
+  userId: string,
+  disciplineId: string
+) => {
   const userDoc = doc(db, "users", userId);
   const disciplinesRef = collection(userDoc, "disciplines");
   const disciplineDoc = doc(disciplinesRef, disciplineId);
   const setsRef = collection(disciplineDoc, "sets");
   const data = await getDocs(setsRef);
-  const mappedData: DataPoint[] = data.docs.map((doc) => {  
+  const mappedData: DataPoint[] = data.docs.map((doc) => {
     const setData = doc.data();
     return {
-      timeStamp: setData.timeStamp.toDate()
+      timeStamp: setData.timeStamp.toDate(),
     };
   });
 
-const lastSetDate = new Date(Math.max(...mappedData.map(set => set.timeStamp.getTime())));
-return lastSetDate;
-}
-export const getGoals = async (userId: string, disciplineId: string): Promise<{day: number, week: number, month: number, year: number} | null> => {
+  const lastSetDate = new Date(
+    Math.max(...mappedData.map((set) => set.timeStamp.getTime()))
+  );
+  return lastSetDate;
+};
+export const getGoals = async (
+  userId: string,
+  disciplineId: string
+): Promise<{
+  day: number;
+  week: number;
+  month: number;
+  year: number;
+} | null> => {
   const userDoc = doc(db, "users", userId);
   const disciplinesRef = collection(userDoc, "disciplines");
   const disciplineDoc = doc(disciplinesRef, disciplineId);
@@ -114,12 +133,16 @@ export const getGoals = async (userId: string, disciplineId: string): Promise<{d
     const weeklyGoal = userData.weeklyGoal || 0;
     const monthlyGoal = userData.monthlyGoal || 0;
     const yearlyGoal = userData.yearlyGoal || 0;
-    return {day: dailyGoal, week: weeklyGoal, month: monthlyGoal, year: yearlyGoal};
+    return {
+      day: dailyGoal,
+      week: weeklyGoal,
+      month: monthlyGoal,
+      year: yearlyGoal,
+    };
   } else {
     return null;
   }
-  
-}
+};
 
 export const getDayStartHour = async (userId: string) => {
   const userDoc = doc(db, "users", userId);
@@ -130,27 +153,37 @@ export const getDayStartHour = async (userId: string) => {
     if (dayStart != undefined) {
       return dayStart;
     }
-     return 0;
+    return 0;
   } else {
     return 0;
   }
-}
+};
 
 //update
 export const addDiscipline = async (userId: string, newDiscipline: string) => {
   const userDoc = doc(db, "users", userId);
   const disciplinesRef = collection(userDoc, "disciplines");
-  await addDoc(disciplinesRef, { name: newDiscipline, dailyGoal: 0, weeklyGoal: 0, monthlyGoal: 0, yearlyGoal: 0 });
-}
+  await addDoc(disciplinesRef, {
+    name: newDiscipline,
+    dailyGoal: 0,
+    weeklyGoal: 0,
+    monthlyGoal: 0,
+    yearlyGoal: 0,
+  });
+};
 export const setDayStartHour = async (userId: string, startHour: number) => {
   const userDoc = doc(db, "users", userId);
   await updateDoc(userDoc, {
-    dayStartHour: startHour
+    dayStartHour: startHour,
   });
-}
+};
 
-export const addGoal = async (userId: string, disciplineId: string, type:string, targetReps:number) =>
-{
+export const addGoal = async (
+  userId: string,
+  disciplineId: string,
+  type: string,
+  targetReps: number
+) => {
   const userDoc = doc(db, "users", userId);
   const disciplinesRef = collection(userDoc, "disciplines");
   const disciplineDoc = doc(disciplinesRef, disciplineId);
@@ -158,17 +191,21 @@ export const addGoal = async (userId: string, disciplineId: string, type:string,
   const fieldToUpdate = type + "Goal"; // type will be one of "daily", "weekly", "monthly", "yearly"
 
   await updateDoc(disciplineDoc, {
-    [fieldToUpdate]: targetReps
+    [fieldToUpdate]: targetReps,
   });
-}
+};
 
-export const addSet = async (userId: string, disciplineId: string, set: { timeStamp: Date, reps: number}) => {
+export const addSet = async (
+  userId: string,
+  disciplineId: string,
+  set: { timeStamp: Date; reps: number }
+) => {
   const userDoc = doc(db, "users", userId);
   const disciplinesRef = collection(userDoc, "disciplines");
   const disciplineDoc = doc(disciplinesRef, disciplineId);
   const setsRef = collection(disciplineDoc, "sets");
-  await addDoc(setsRef, set)
-}
+  await addDoc(setsRef, set);
+};
 
 //delete
 export const deleteUser = async (id: string) => {
